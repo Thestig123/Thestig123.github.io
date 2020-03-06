14de2bf390cf7be4855b7b7020c6735854f6266c
Thestig123 <israelflores9595@gmail.com>
2020-03-06 00:17:57 -0700

    activity

--- lesson9/js/prophets.js
+++ lesson9/js/prophets.js
@@ -5,5 +5,30 @@ fetch(requestURL)
     return response.json();
   })
   .then(function (jsonObject) {
-    console.table(jsonObject);  // temporary checking for valid response and data parsing
+    //console.table(jsonObject);  // temporary checking for valid response and data parsing
+    const prophets = jsonObject['prophets'];
+
+    prophets.forEach(prophet => {
+      let card = document.createElement('section');
+      let h2 = document.createElement('h2');
+      let p = document.createElement('p');
+      let image = document.createElement('img');
+
+
+      let fullname = `${prophet.name} ${prophet.name}`;
+      let dateofbirth = `${prophet.birthdate}`;
+      let placeofbirth = `${prophet.birthplace}`;
+      
+      h2.innerHTML = fullname;
+      p.innerHTML = dateofbirth + '<br>' + placeofbirth;
+
+      image.setAttribute('src', prophet.imageurl);
+      image.setAttribute('alt', fullname);
+
+      card.appendChild(h2);
+      card.appendChild(p);
+      card.appendChild(image);
+
+      document.querySelector('.cards').appendChild(card);
+    });
   });
